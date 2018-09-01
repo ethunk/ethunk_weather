8520e3ba8b50af9338b6767673ad01c2252b2ef6
ethunk <ethunk>
2018-08-31 20:23:03 -0400

    update

--- db/migrate/20180826224139_create_current_weathers.rb
+++ db/migrate/20180826224139_create_current_weathers.rb
@@ -1,7 +1,20 @@
 class CreateCurrentWeathers < ActiveRecord::Migration[5.1]
   def change
-    create_table :current_weathers do |t|
-
+    create_table :current_weathers, {id: false}  do |t|
+      t.json :coord
+      t.json :weather
+      t.string :base
+      t.json :main
+      t.json :wind
+      t.json :clouds
+      t.json :rain
+      t.json :snow
+      t.integer :dt
+      t.integer :id, null: false, index: {unique: true}
+      t.string :name
+      t.string :cod
+      t.string :visibility
+      t.json :sys
       t.timestamps
     end
   end
